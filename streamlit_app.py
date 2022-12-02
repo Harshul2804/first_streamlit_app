0505e7a84e857c056f3247c28a74c0184fac7b79
Harshul Bhaliya <33797388+Harshul2804@users.noreply.github.com>
2022-12-02 15:22:56 +0530

    My first typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit;
 streamlit.title('My Parents new healthy Dinner')
-streamlit.heaeder('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit('Kale Spinach and Rocket Smoothie')
-streamlit('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
