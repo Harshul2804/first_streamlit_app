b2b56ad13d34a33f3d4e6687e88f0b4ae04b1769
Harshul Bhaliya <33797388+Harshul2804@users.noreply.github.com>
2022-12-02 15:21:48 +0530

    Adding more to steamlitApp.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('My Parents new healthy Dinner')
+streamlit.heaeder('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit('Kale Spinach and Rocket Smoothie')
+streamlit('Hard-Boiled Free-Range Egg')
